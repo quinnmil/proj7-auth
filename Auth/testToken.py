398bd05e0adc84d513e3c5daa319df2f5e546e39
quinnmil <quinnmil@gmail.com>
2018-03-12 21:07:14 -0700

    Add register and get_token method. Can create new users through curl.

--- Auth/testToken.py
+++ Auth/testToken.py
@@ -1,6 +1,9 @@
 from itsdangerous import (TimedJSONWebSignatureSerializer \
                                   as Serializer, BadSignature, \
                                   SignatureExpired)
+
+from flask import requests
+import flask
 import time
 
 # initialization
@@ -23,8 +26,37 @@ def verify_auth_token(token):
         return None    # invalid token
     return "Success"
 
-if __name__ == "__main__":
-    t = generate_auth_token(10)
-    for i in range(1, 20):
-	print verify_auth_token(t)
-        time.sleep(1)
+    
+def token_required(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    message = None
+    authHeader = request.headers.get("Authorization")
+    if authHeader == None:
+      message = "No HTTP"
+    else:
+      try:
+        authMode, authString = authHeader.split(" ", 1)
+      except ValueError:
+        message = "Bad auth string"
+    if message != None:
+      return {"message":message}, 401
+    userPass = b64decode(authString)
+    try:
+      theToken, password = userPass.decode().split(":", 1)
+    except ValueError:
+      message = "Bad auth string"
+    if message != None:
+      return flask.jsonify({"message":message}), 401
+
+    if verify_auth_token(theToken):
+      return f(*args, **kwargs)
+    else:
+      return {"message":"bad token"}, 401
+  return decorated_function
+
+# if __name__ == "__main__":
+#     t = generate_auth_token(10)
+#     for i in range(1, 20):
+# 	print verify_auth_token(t)
+#         time.sleep(1)
